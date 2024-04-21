8ae6e70c9fe60bf7d58edd58495907d0503937d9
Scartissue <68987475+imscartissue@users.noreply.github.com>
2024-04-21 10:39:58 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let computerWins = 0;
 let playerWins = 0;
 let history = new Array();
+let playAgain = true;
 
 // Returns either "rock", "paper" or "scissors"
 function getComputerChoice() {
@@ -71,7 +72,7 @@ function playRound(playerSelection, computerSelection) {
 
     paragraphContainer.appendChild(history[history.length - 1]);
 
-    console.log(result);
+    // console.log(result);
     return result;
 }
 
@@ -81,83 +82,102 @@ container.setAttribute("style", `width: ${window.innerWidth - 20}px; height: ${w
 
 
 // Click events handled perfectly
-const buttonContainer = document.querySelector(".button-container");
-buttonContainer.addEventListener('click', (event) => {
+if (playAgain) {
 
-    let computerChoice = getComputerChoice();
-    const computerButton = document.querySelector("#computer");
-    const score = document.querySelector(".score");
 
-    let emoji;
-    switch (computerChoice) {
-        case "rock":
-            emoji = "🪨";
-            break;
-        case "paper":
-            emoji = "📄";
-            break;
-        case "scissors":
-            emoji = "✂️";
-            break;
-    }
-    computerButton.textContent = emoji;
+    const buttonContainer = document.querySelector(".button-container");
+    buttonContainer.addEventListener('click', (event) => {
 
-    let result;
+        if (playAgain == false) { return; }
 
-    switch (event.target.id) {
-        case "rock":
-            result = playRound("rock", computerChoice);
-            break;
-        case "paper":
-            result = playRound("paper", computerChoice);
-            break;
-        case "scissors":
-            result = playRound("scissors", computerChoice);
-            break;
-    }
-
-    if (result != "") {
-
-        if (result.slice(4, 5) == 'w') {
-            playerWins++;
-            score.textContent = `${playerWins}-${computerWins}`;
-        } else if (result.slice(4, 5) == 'l') {
-            computerWins++;
-            score.textContent = `${playerWins}-${computerWins}`;
-        } else {
-            score.textContent = `${playerWins}-${computerWins}`;
-        }
-    }
-
-    if (playerWins == 5 || computerWins == 5) {
-        let endMessage;
-        if (playerWins == 5) {
-            endMessage = "You won the game!";
-            const scoreSibling = document.createElement("p");
-            scoreSibling.textContent = endMessage;
-            score.appendChild(scoreSibling);
-        } else if (computerWins == 5) {
-            endMessage = "You lost the game!";
-            const scoreSibling = document.createElement("p");
-            scoreSibling.textContent = endMessage;
-            score.appendChild(scoreSibling);
-        }
+        let computerChoice = getComputerChoice();
         const computerButton = document.querySelector("#computer");
-        computerButton.textContent = "🤔";
-        alert("The game is over");
-        playerWins = 0;
-        computerWins = 0;
-        for (let i = 0; i < history.length; i++) {
-            history[i].textContent = "";
+        const score = document.querySelector(".score");
+
+        let emoji;
+        switch (computerChoice) {
+            case "rock":
+                emoji = "Rock 🪨";
+                break;
+            case "paper":
+                emoji = "Paper 📄";
+                break;
+            case "scissors":
+                emoji = "Scissors ✂️";
+                break;
         }
-        history = new Array();
-        setTimeout(() => {
-            score.textContent = "0-0";
-        }, 1500);
-    }
+        computerButton.textContent = emoji;
 
-    // container.setAttribute("style", "display: flex; justify-content: space-evenly;");
-});
+        let result;
 
+        switch (event.target.id) {
+            case "rock":
+                result = playRound("rock", computerChoice);
+                break;
+            case "paper":
+                result = playRound("paper", computerChoice);
+                break;
+            case "scissors":
+                result = playRound("scissors", computerChoice);
+                break;
+        }
 
+        if (result != "") {
+
+            if (result.slice(4, 5) == 'w') {
+                playerWins++;
+                score.textContent = `${playerWins}-${computerWins}`;
+            } else if (result.slice(4, 5) == 'l') {
+                computerWins++;
+                score.textContent = `${playerWins}-${computerWins}`;
+            } else {
+                score.textContent = `${playerWins}-${computerWins}`;
+            }
+        }
+
+        if (playerWins == 5 || computerWins == 5) {
+            let endMessage;
+            if (playerWins == 5) {
+                endMessage = "You won the game!";
+                const scoreSibling = document.createElement("p");
+                scoreSibling.textContent = endMessage;
+                score.appendChild(scoreSibling);
+            } else if (computerWins == 5) {
+                endMessage = "You lost the game!";
+                const scoreSibling = document.createElement("p");
+                scoreSibling.textContent = endMessage;
+                score.appendChild(scoreSibling);
+            }
+            const computerButton = document.querySelector("#computer");
+            computerButton.textContent = "Thinking 🤔";
+            if (playAgain) {
+                playAgain = false;
+                // console.log(playAgain);
+                // alert("The game is over");
+                const playAgainButton = document.createElement("button");
+                playAgainButton.textContent = "Play again?";
+                playAgainButton.setAttribute("style", "color: #fbeceb; background-color: #08a045; font-size: 20px; padding: 12px;");
+                playAgainButton.setAttribute("onMouseOver", "this.style.backgroundColor = '#035825'");
+                playAgainButton.setAttribute("onMouseOut", "this.style.backgroundColor = '#08a045'");
+                score.setAttribute("style", "display: flex; justify-content: space-around; flex-wrap: wrap;");
+                score.appendChild(playAgainButton);
+                playAgainButton.addEventListener('click', () => {
+                    playAgain = true;
+                    // console.log(playAgain);
+                    playerWins = 0;
+                    computerWins = 0;
+                    for (let i = 0; i < history.length; i++) {
+                        history[i].textContent = "";
+                    }
+                    history = new Array();
+                    score.textContent = "0-0";
+                    score.removeChild(playAgainButton);
+                });
+            }
+        }
+
+        // container.setAttribute("style", "display: flex; justify-content: space-evenly;");
+    });
+
+}
 
